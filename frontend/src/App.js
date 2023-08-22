e61039dbc5eff6f2db3d45aeac86efe24b3cfa31
Dimitar Milkov <dimemilkov1@gmail.com>
2023-08-22 16:26:50 +0200

    made some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Nav from 'react-bootstrap/Nav';
 import { Store } from './Store';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   const { state } = useContext(Store);
@@ -40,6 +41,7 @@ function App() {
           <Container className='mt-3'>
             <Routes>
               <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path='/cart' element={<CartScreen />} />
               <Route path='/' element={<HomeScreen />} />
             </Routes>
           </Container>
