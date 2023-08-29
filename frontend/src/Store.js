e0a5f4b7dc683e64e1c84b6659c0cd0f4195000c
Dimitar Milkov <dimemilkov1@gmail.com>
2023-08-29 15:01:02 +0200

    new logic implemented

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -4,7 +4,10 @@ export const Store = createContext();
 
 const initialState = {
   cart: {
-    cartItems: [],
+    //if cartItems exist in the local storage use JSON.parse to convert 'cartItems' to js object, otherwise set it to empty array
+    cartItems: localStorage.getItem('cartItems')
+      ? JSON.parse(localStorage.getItem('cartItems'))
+      : [],
   },
 };
 function reducer(state, action) {
@@ -20,9 +23,16 @@ function reducer(state, action) {
             item._id === existItem._id ? newItem : item
           )
         : [...state.cart.cartItems, newItem];
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     //if we have already existItem in the card, we need to use map function on the cartItem to update the current item with the new item which we get from the line 14(aciton.payload) otherwise keep the previous item in the cart. If the existItem is null, it means that it's new item that has to be added to the array (line 23)
-
+    case 'CART_REMOVE_ITEM': {
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
+      localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      return { ...state, cart: { ...state.cart, cartItems } };
+    }
     default:
       return state;
   }
