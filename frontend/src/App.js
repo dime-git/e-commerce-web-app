f8401c67adce3cf48ae830a92aed8a05b822ee53
Dimitar Milkov <dimemilkov1@gmail.com>
2023-08-30 17:37:46 +0200

    imported payment method

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,8 +13,9 @@ import { useContext } from 'react';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
-import SignupScreen from './screens/SignupScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
+import SignupScreen from './screens/SignupScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -24,6 +25,7 @@ function App() {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
   };
   return (
     <BrowserRouter>
@@ -81,6 +83,7 @@ function App() {
                 path='/shipping'
                 element={<ShippingAddressScreen />}
               ></Route>
+              <Route path='/payment' element={<PaymentMethodScreen />}></Route>
               <Route path='/' element={<HomeScreen />} />
             </Routes>
           </Container>
