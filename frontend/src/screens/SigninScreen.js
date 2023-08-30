a201547a79237b5e3834189e86ab65f473fa9cf5
Dimitar Milkov <dimemilkov1@gmail.com>
2023-08-30 16:52:37 +0200

    changed the signin screen

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,9 +1,9 @@
+import Axios from 'axios';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { Helmet } from 'react-helmet-async';
-import Axios from 'axios';
 import { useContext, useEffect, useState } from 'react';
 import { Store } from '../Store';
 import { toast } from 'react-toastify';
@@ -19,22 +19,19 @@ export default function SigninScreen() {
   const [password, setPassword] = useState('');
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
-
   const { userInfo } = state;
-
   const submitHandler = async (e) => {
     e.preventDefault();
-
     try {
       const { data } = await Axios.post('/api/users/signin', {
         email,
         password,
       });
-      ctxDispatch({ type: 'USER SIGNIN', payload: data });
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
       localStorage.setItem('userInfo', JSON.stringify(data));
       navigate(redirect || '/');
-    } catch (error) {
-      toast.error(getError(error));
+    } catch (err) {
+      toast.error(getError(err));
     }
   };
 
