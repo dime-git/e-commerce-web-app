e3238fab26cef325c67d8c851eb7ac4d1f182a43
Dimitar Milkov <dimemilkov1@gmail.com>
2023-08-30 17:38:30 +0200

    set default value for payment method

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -11,6 +11,9 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
@@ -38,16 +41,18 @@ function reducer(state, action) {
       localStorage.setItem('cartItems', JSON.stringify(cartItems));
       return { ...state, cart: { ...state.cart, cartItems } };
     }
-    case 'USER_SIGNIN': {
+    case 'USER_SIGNIN':
       return { ...state, userInfo: action.payload };
-    }
-    case 'USER_SIGNOUT': {
+    case 'USER_SIGNOUT':
       return {
         ...state,
         userInfo: null,
-        cart: { cartItems: [], shippingAddress: {} },
+        cart: {
+          cartItems: [],
+          shippingAddress: {},
+          paymentMethod: '',
+        },
       };
-    }
     case 'SAVE_SHIPPING_ADDRESS':
       return {
         ...state,
@@ -55,6 +60,11 @@ function reducer(state, action) {
           ...state.cart,
           shippingAddress: action.payload,
         },
+      };
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
       };
     default:
       return state;
