b9c8eee2f48887795403b1be2ed563783887c2ef
Dimitar Milkov <dimemilkov1@gmail.com>
2023-08-31 17:00:12 +0200

    return clientId for paypal

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('connected to DB');
+    console.log('connected to db');
   })
   .catch((err) => {
     console.log(err.message);
@@ -23,17 +23,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-const port = process.env.PORT || 5000;
-
-app.use((err, res, req, next) => {
+app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`port starting at http://localhost:${port}`);
+  console.log(`serve at http://localhost:${port}`);
 });
