a8586c273aa6e58d8e20df49762a314f3f0b7d7e
Dimitar Milkov <dimemilkov1@gmail.com>
2023-08-31 14:53:37 +0200

    created middleware

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,18 @@ export const generateToken = (user) => {
     }
   );
 };
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET, (error, decode) => {
+      if (error) {
+        res.status(404).send({ message: 'Invalid Token' });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  }
+};
